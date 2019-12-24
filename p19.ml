f60d5df2df4cb552ecd658d000675ecd689a4513
Ruijie Fang <ruijief@princeton.edu>
2019-12-24 18:49:10 +0800

    P19

--- p19.ml
+++ p19.ml
@@ -8,16 +8,28 @@
   - : string list = ["g"; "h"; "a"; "b"; "c"; "d"; "e"; "f"]
 *)
 
+let rec split l n = 
+  let rec do_split = fun l i ->
+    match l with
+      | [] -> ([], [])
+      | a :: t ->
+        let (list1, list2) = (do_split t (i + 1)) in
+          if i >= n then (list1, a :: list2)
+          else (a :: list1, list2)
+  in
+    do_split l 0
+;; 
+
 let rec rotate l k = 
   if k = 0 then l
   else
-    match l with
-      | [] -> []
-      | a :: t ->
-        if k > 0 then (rotate (t @ [a]) (k - 1))
-        else (rotate )
+    if k > 0 then
+      match l with
+        | [] -> []
+        | a :: t -> (rotate (t @ [a]) (k - 1))
     else
-      let j = -k in
-        
+      let len = List.length l in
+        let (left, right) = (split l (len + k)) in
+          right @ left
 ;;
 
