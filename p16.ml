b6d630a52ce826954c2a1a3386db067446cd66f9
Ruijie Fang <ruijief@princeton.edu>
2019-12-24 12:16:21 +0800

    fix p16

--- p16.ml
+++ p16.ml
@@ -3,18 +3,25 @@
 - : string list = ["a"; "b"; "d"; "e"; "g"; "h"; "j"]
 *)
 
+open Printf;;
+
+(* drop l n
+  drops every n-th element from list l *)
 let rec drop l n = 
-  let rec do_drop = fun l k ->
-    match k with 
-      | 1 -> 
-        match l with
-          | [] -> ([], [])
-          | a :: t -> t
-      | j -> 
-        match l with
-          | [] -> []
-          | a :: t -> a :: (do_drop l (k - 1))
+  let rec do_drop = fun l k n ->
+    if k == n then
+      match l with
+        | [] -> []
+        | a :: t -> do_drop t 1 n
+    else
+      match l with
+        | [] -> []
+        | a :: t -> 
+          a :: (do_drop t (k + 1) n)
   in
-    match (do_drop l n) with
-      | [] -> []
-      | a :: t -> 
+    do_drop l 1 n
+;;
+
+
+
+
